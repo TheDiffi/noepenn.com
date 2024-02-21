affbe52ad2c8a6e89d4a9a0af041f21b392ce174
NoePenn <noepenn1234@gmail.com>
2024-02-21 22:44:47 +0100

    l

--- javascript.js
+++ javascript.js
@@ -1,10 +1,55 @@
-function menuOnClick() {
-  document.getElementById("menu-bar").classList.toggle("change");
-  document.getElementById("nav").classList.toggle("change");
-  document.getElementById("menu-bg").classList.toggle("change-bg");
+var clickTimeright = 0
+
+document.getElementById("buttonRightSection1")
+.onclick = function(){
+  horizontalButtonRightSection1()
+ 
+};
+
+
+function horizontalButtonRightSection1() {
+  var container = document.getElementById("maincontainer");
+  container.style.transform = `translateX(${-100}vw)`
+  
 }
 
-//document.getElementById("menu-bar").addEventListener{}//
+document.getElementById("buttonRightSection2")
+.onclick = function(){
+  horizontalButtonRightSection2()
+ 
+};
 
 
+function horizontalButtonRightSection2() {
+  var container = document.getElementById("maincontainer");
+  container.style.transform = `translateX(${-200}vw)`
+  
+}
+
+
+document.getElementById("buttonLeftSection2")
+.onclick = function(){
+  horizontalButtonLeft()
+  
+};
+
+
+function horizontalButtonLeft() {
+  var container = document.getElementById("maincontainer");
+  container.style.transform = `translateX(${0}vw)`
+  
+}
+
+document.getElementById("buttonLeftSection3")
+.onclick = function(){
+  horizontalButtonLeftSection3()
+  
+};
+
+
+function horizontalButtonLeftSection3() {
+  var container = document.getElementById("maincontainer");
+  container.style.transform = `translateX(${-100}vw)`
+  
+}
 
